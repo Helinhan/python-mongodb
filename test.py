84fa9ebab6747181bc9561e2ca9390f857089a92
helinhan <helinhan@hantongy.com>
2018-06-03 10:42:47 +0800

    安装了mongdb 扩展 pymongo

--- test.py
+++ test.py
@@ -1,26 +1,28 @@
 import top.api
+from pymongo import *
 
-req = top.api.TbkItemGetRequest()
-req.set_app_info(top.appinfo(24533259, "58e00610ceeaafed2d8ba7ab59c2d3ac"))
 
-req.fields = "num_iid,title,pict_url,small_images,reserve_price,zk_final_price,user_type,provcity,item_url,seller_id,volume,nick"
-req.q = "女装"
-#req.cat = "16,18"
-#req.itemloc = "杭州"
-# req.sort = "tk_rate_des"
-# req.is_tmall = "false"
-# req.is_overseas = "false"
-# req.start_price = 10
-# req.end_price = 10
-# req.start_tk_rate = 123
-# req.end_tk_rate = 123
-# req.platform = 1
-# req.page_no = 1
-# req.page_size = 20
-resp = req.getResponse()
-print(resp)
-# try:
-#     resp = req.getResponse()
-#     print(resp)
-# except Exception as e:
-#     print(e)
+'''
+链接数据库
+'''
+def insert():
+    try:
+        client=MongoClient(host="localhost",port=27017)# 创建连接对象
+        db=client.goods#获取数据库
+        return db
+    except Exception as e:
+        print(e)
+
+
+req = top.api.TbkUatmFavoritesGetRequest()
+req.set_app_info(top.appinfo(24533259,"58e00610ceeaafed2d8ba7ab59c2d3ac"))
+
+req.page_no = 1
+req.page_size = 20
+req.fields = "favorites_title,favorites_id,type"
+req.type = 1
+try:
+    resp = req.getResponse()
+    print(resp)
+except Exception as e:
+    print(e)
